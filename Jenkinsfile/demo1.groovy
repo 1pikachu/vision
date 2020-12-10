c73f628f57b0d175ef0ff3f8fc745da5f659f449
1pikachu <793654486@qq.com>
2020-12-10 11:34:39 +0800

    add jenkins demo groovy

---
+++ Jenkinsfile/demo1.groovy
@@ -0,0 +1,28 @@
+node('master')
+{
+	stage('build env'){
+		sh '''
+			# source pythonenv
+			echo "prepare pythonenv"
+			python --version
+			# build proj
+			echo "build start"
+			# check proj whether build
+			echo "the proj build success"
+		'''
+	}
+	stage('run test'){
+		sh '''
+			# begin test
+			echo "test start"
+			# get test exit code
+			echo 'test exit code is: "$?
+		'''
+	}
+	stage('collect log'){
+		sh '''
+			# collect log
+			echo "collect log fininshed"
+		'''
+	}
+}
